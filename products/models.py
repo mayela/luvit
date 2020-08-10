d0b7f78f416d801e4272da0cd8325be0df8fe5b7
Maricela Sanchez <mayela0x14@gmail.com>
2020-08-10 17:36:10 -0500

    Create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+'''
+Product {
+    id: String
+    name: String
+    value: Float
+    discount_value: Float?
+    stock: Int
+}
+'''
+
+class Product(models.Model):
+    '''
+    Class representing all the fields needed to save the info of a product
+    '''
+    name = models.CharField(max_length=100)
+    value = models.FloatField()
+    discount_value = models.FloatField()
+    stock = models.IntegerField()
