1e8c80e450c1b3faf0532632fe845aad521616cf
Maricela Sanchez <mayela0x14@gmail.com>
2020-08-10 20:09:12 -0500

    Add get all products view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Product
+from .schemas import ProductSchema
+
+def get_all_products(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        schema = ProductSchema(many=True)
+        products_data = schema.dump(products)
+        return JsonResponse(data={"products": products_data})
