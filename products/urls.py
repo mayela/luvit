aa4c79bf360b18c127cf7728baecb8a0033a7330
Maricela Sanchez <mayela0x14@gmail.com>
2020-08-12 10:15:03 -0500

    Refactor urls files

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from .views import get_all_products, create_products_bulk
 
+app_name = 'products'
+
 urlpatterns = [
-    path('', get_all_products),
-    path('bulk_insert/', create_products_bulk)
+    path('', get_all_products, name="all-products"),
+    path('bulk_insert/', create_products_bulk, name="bulk-insert")
 ]
